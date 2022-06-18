8e50967da44fbf4ba8dd82e8eaa4bd64bd93ea8b
Joshua Rei <josh@redvanworkshop.com>
2022-06-18 13:24:03 -0400

    Larger Refactor
    I was playing around with CSS in JS solutions Tailwind,  Sass I enjoy leveraging bootstrap class based styling. This led me into what other ways I might be able to manage the data modal that is readable and had the commenting and syntax. So this is a take on using yaml as the one source of truth data modal for Oday so far testing has been good performance from trimming the outside JSON library for internal Deno readTextFile as well as using encoding yaml to translate the files which is also a base Deno method.

--- views/pages/Home/home.js
+++ views/pages/Home/home.js
@@ -1,54 +1,40 @@
-import loadJsonFile from "https://deno.land/x/load_json_file@v1.0.0/mod.ts";
-import { BaseStyles } from "../../../styles.js";
+import FooterTemplate from "../../partials/footer.js";
 
 export default async () => {
-    const data = await loadJsonFile("views/pages/Home/home.json");
-    const { Footer, Main, NavLinks, Header } = data;
+    const Oday = window.Oday;
 
     return `
-        <style>
-            ${BaseStyles()}
-        </style>
-        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-          <img id="logo" src="${Header.Logo.Url.Png.url}" class="mt-5 mb-2 w-25 mx-auto" alt="logo image">
-          <div class="header-container">
-              <header class="mb-auto">
-                <div>
-                  <h2 class="${Header.Classes}">${Header.PrimaryText}</h2>
-                  <nav class="my-3 nav nav-masthead justify-content-center float-md-end">
-                    <a class="nav-link active ${NavLinks.Home.Classes}" aria-current="page" href="${NavLinks.Home.Href}">
-                        ${NavLinks.Home.PrimaryText}
-                    </a>
-                    <a class="${NavLinks.About.Classes}" href="${NavLinks.About.Href}">
-                        ${NavLinks.About.PrimaryText}
-                    </a>
-                  </nav>
-                </div>
-              </header>
-            <h2>Small Fast Measured</h2>
-            <small>Curious you should be</small>
-          </div>
+        <div class="${Oday.Home.classes}">
+          <img id="logo" src="${Oday.Layout.Logo.href}" class="${Oday.Layout.Logo.classes}" alt="${Oday.Layout.Logo.alt}">
+          <header class="mb-auto">
+          <nav id="${Oday.Layout.Navigation.id}" class="${Oday.Layout.Navigation.classes}">
+            <a class="${Oday.Layout.Navigation.content.Home.classes} active" aria-current="page" href="${Oday.Layout.Navigation.content.Home.href}">
+                ${Oday.Layout.Navigation.content.Home.content.primary}
+            </a>
+            <a class="${Oday.Layout.Navigation.content.About.classes}" href="${Oday.Layout.Navigation.content.About.href}">
+                ${Oday.Layout.Navigation.content.About.content.primary}
+            </a> 
+          </nav>
+          </header>
+        <h2>${Oday.Home.Header.content.primary}</h2>
+        <small>${Oday.Home.Header.content.secondary}</small>
 
-        <div class="cover-container d-flex w-35 h-100 p-3 mx-auto flex-column">
-          <a class="btn btn-primary border-dark" data-toggle="collapse" href="#collapse-section" role="button" aria-expanded="false" aria-controls="collapseExample">
+        <div class="p-3">
+          <a class="btn btn-primary border-dark" data-bs-toggle="collapse" data-bs-target="#collapse-section" role="button" aria-expanded="false">
               Click me to learn the story of Oday!
           </a>
         </div>
 
-          <main id="collapse-section" class="px-3 collapse">
-            <div class="card text-dark text-left">
-              <div class="card-body">
-                <p class="lead">${Main.PrimaryText}</p>
-              </div>
+          <main class="${Oday.Home.Main.classes}">
+            <div id="collapse-section"  class="card text-dark collapse text-break">
+              <div class="card">
+                  <div class="card-body">
+                    <p class="lead">${Oday.Home.Main.content.primary}</p>
+                  </div>
+                </div>
             </div>
-            <p class="alert-success mt-2 p-2">${Main.SecondaryText}</p>
           </main>
-
-          <footer class="mt-auto">
-            <p>${Footer.PoweredByDeno}</p>
-            <p class="maintainer">${Footer.Maintainer}</p>
-            <small class="copyright">${Footer.Copyright}</small>
-          </footer>
+          ${await FooterTemplate()}
         </div>
     `;
 };
